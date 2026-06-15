f514a9e33758646c1fece22eb6315df1c0bbb518
B. Szonye <bszonye@gmail.com>
2026-06-15 00:17:27 -0700

    add alternate query to rebuild all roads

--- data/bz-trails-always.sql
+++ data/bz-trails-always.sql
@@ -3,3 +3,8 @@ INSERT OR IGNORE INTO Constructible_FreeRoads(ConstructibleType)
     ConstructibleClass != "IMPROVEMENT" AND
     TypeTags.Tag = "AGELESS" AND
     TypeTags.Type = Constructibles.ConstructibleType;
+
+-- alternate query to rebuild all roads during age transition
+-- INSERT OR IGNORE INTO Constructible_FreeRoads(ConstructibleType)
+--     SELECT ConstructibleType FROM Constructibles
+--     WHERE ConstructibleClass != "IMPROVEMENT";
